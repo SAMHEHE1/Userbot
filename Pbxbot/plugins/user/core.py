03132c714befdca5bcdcf634a4262e825b7fcffc
SAMHEHE1 <sampawaryt@gmail.com>
2026-05-07 10:20:06 +0530

    Update core.py

--- Pbxbot/plugins/user/core.py
+++ Pbxbot/plugins/user/core.py
@@ -70,7 +70,7 @@ async def repo(client: Client, message: Message):
 async def inline_repo(client: Client, inline_query):
     buttons = [
         [
-            InlineKeyboardButton("ʀᴇᴘᴏ", url="https://gitlab.com/Badhacker98/PBX_2.0/-/forks/new")
+            InlineKeyboardButton("ʀᴇᴘᴏ", url="t.me/kaisenbots")
         ],
         [
             InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇs", url="https://t.me/PBX_UPDATE"),
