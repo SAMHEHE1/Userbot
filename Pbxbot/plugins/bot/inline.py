c33467b92d5315feb9922f5db161782bf96ef718
SAMHEHE1 <sampawaryt@gmail.com>
2026-05-07 07:58:05 +0530

    Update help menu title in inline.py

--- Pbxbot/plugins/bot/inline.py
+++ Pbxbot/plugins/bot/inline.py
@@ -26,7 +26,7 @@ async def help_inline(_, query: InlineQuery):
         results=[
             (
                 InlineQueryResultArticle(
-                    "Pbxbot 2.0 Help Menu 👻",
+                    "Oᴡɴ Usᴇʀʙᴏᴛ Help Menu 👻",
                     InputTextMessageContent(
                         caption,
                         disable_web_page_preview=True,
